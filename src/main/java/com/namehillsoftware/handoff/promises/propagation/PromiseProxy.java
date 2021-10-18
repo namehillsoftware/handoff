a28a4fccced7b79434cd3aedd90c891be5c705fb
David Vedvick <dvedvick@gmail.com>
2021-10-18 11:58:33 -0500

    Use single instance of proxies

--- src/main/java/com/namehillsoftware/handoff/promises/propagation/PromiseProxy.java
+++ src/main/java/com/namehillsoftware/handoff/promises/propagation/PromiseProxy.java
@@ -6,16 +6,18 @@ import com.namehillsoftware.handoff.promises.Promise;
 
 public class PromiseProxy<Resolution> {
 
-	private final Messenger<Resolution> messenger;
+	private final ResolutionProxy<Resolution> resolutionProxy;
+	private final RejectionProxy rejectionProxy;
 	private final CancellationProxy cancellationProxy = new CancellationProxy();
 
 	public PromiseProxy(Messenger<Resolution> messenger) {
-		this.messenger = messenger;
+		this.resolutionProxy = new ResolutionProxy<>(messenger);
+		this.rejectionProxy = new RejectionProxy(messenger);
 		messenger.cancellationRequested(cancellationProxy);
 	}
 
 	public void proxy(Promise<Resolution> promise) {
-		promise.then(new ResolutionProxy<>(messenger), new RejectionProxy(messenger));
+		promise.then(resolutionProxy, rejectionProxy);
 
 		cancellationProxy.doCancel(promise);
 	}
