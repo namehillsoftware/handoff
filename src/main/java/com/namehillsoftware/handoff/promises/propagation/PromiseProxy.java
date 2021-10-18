76a5a28f3b4fcd486ad234a0cd675ec1ee606e5d
David Vedvick <dvedvick@gmail.com>
2021-10-18 12:03:19 -0500

    Make `PromiseProxy` final

--- src/main/java/com/namehillsoftware/handoff/promises/propagation/PromiseProxy.java
+++ src/main/java/com/namehillsoftware/handoff/promises/propagation/PromiseProxy.java
@@ -4,15 +4,15 @@ package com.namehillsoftware.handoff.promises.propagation;
 import com.namehillsoftware.handoff.Messenger;
 import com.namehillsoftware.handoff.promises.Promise;
 
-public class PromiseProxy<Resolution> {
+public final class PromiseProxy<Resolution> {
 
+	private final CancellationProxy cancellationProxy = new CancellationProxy();
 	private final ResolutionProxy<Resolution> resolutionProxy;
 	private final RejectionProxy rejectionProxy;
-	private final CancellationProxy cancellationProxy = new CancellationProxy();
 
 	public PromiseProxy(Messenger<Resolution> messenger) {
-		this.resolutionProxy = new ResolutionProxy<>(messenger);
-		this.rejectionProxy = new RejectionProxy(messenger);
+		resolutionProxy = new ResolutionProxy<>(messenger);
+		rejectionProxy = new RejectionProxy(messenger);
 		messenger.cancellationRequested(cancellationProxy);
 	}
 
