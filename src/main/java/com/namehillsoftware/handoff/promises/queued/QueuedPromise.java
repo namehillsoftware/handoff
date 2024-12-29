0b4535ce46a8ebe3e88a2d6388048b5c907d579a
David <dvedvick@gmail.com>
2024-12-29 10:06:35 -0600

    Added inner messenger that resolves and rejects directly

--- src/main/java/com/namehillsoftware/handoff/promises/queued/QueuedPromise.java
+++ src/main/java/com/namehillsoftware/handoff/promises/queued/QueuedPromise.java
@@ -1,16 +1,20 @@
 package com.namehillsoftware.handoff.promises.queued;
 
-import com.namehillsoftware.handoff.DeferredProxyMessenger;
+import com.namehillsoftware.handoff.Messenger;
+import com.namehillsoftware.handoff.cancellation.Cancellable;
+import com.namehillsoftware.handoff.cancellation.CancellationResponse;
 import com.namehillsoftware.handoff.promises.MessengerOperator;
 import com.namehillsoftware.handoff.promises.Promise;
+import com.namehillsoftware.handoff.promises.propagation.CancellationProxy;
 import com.namehillsoftware.handoff.promises.queued.cancellation.CancellableMessageWriter;
 import com.namehillsoftware.handoff.promises.queued.cancellation.CancellablePreparedMessengerOperator;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class QueuedPromise<Resolution> extends Promise<Resolution> {
 	public QueuedPromise(MessengerOperator<Resolution> task, Executor executor) {
-		super(new Execution.QueuedMessengerResponse<>(task, executor));
+		executor.execute(new QueuedMessenger(task));
 	}
 
 	public QueuedPromise(CancellableMessageWriter<Resolution> task, Executor executor) {
@@ -21,20 +25,52 @@ public class QueuedPromise<Resolution> extends Promise<Resolution> {
 		this(new PreparedMessengerOperator<>(task), executor);
 	}
 
-	private static class Execution {
-		static final class QueuedMessengerResponse<Resolution> extends DeferredProxyMessenger<Resolution> {
+	private final class QueuedMessenger implements
+		Messenger<Resolution>,
+		Runnable,
+		Cancellable {
 
-			private final Executor executor;
+		private final MessengerOperator<Resolution> task;
+		private final CancellationProxy cancellationProxy = new CancellationProxy();
+		private final AtomicReference<CancellationResponse> cancellationResponse = new AtomicReference<>();
 
-			QueuedMessengerResponse(MessengerOperator<Resolution> task, Executor executor) {
-				super(task);
-				this.executor = executor;
+		QueuedMessenger(MessengerOperator<Resolution> task) {
+			this.task = task;
+			cancellationProxy.doCancel(this);
+			QueuedPromise.this.awaitCancellation(cancellationProxy);
+		}
+
+		@Override
+		public void sendResolution(Resolution resolution) {
+			resolve(resolution);
+		}
+
+		@Override
+		public void sendRejection(Throwable error) {
+			reject(error);
+		}
+
+		@Override
+		public void awaitCancellation(CancellationResponse cancellationResponse) {
+			if (this.cancellationResponse.getAndSet(cancellationResponse) == null) {
+				cancellationProxy.doCancel(this);
 			}
+		}
 
-			@Override
-			protected void execute(Runnable runnable) {
-				executor.execute(runnable);
+		@Override
+		public void run() {
+			try {
+				task.send(this);
+			} finally {
+				cancellationResponse.lazySet(null);
 			}
+		}
+
+		@Override
+		public void cancel() {
+			final CancellationResponse cancellationResponse = this.cancellationResponse.get();
+			if (cancellationResponse != null)
+				cancellationResponse.cancellationRequested();
 		}
 	}
 }
